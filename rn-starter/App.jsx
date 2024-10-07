124eeb24780f5c3f74cda18b97c0c41628da02f7
pspujashinde <pspujashinde@gmail.com>
2024-10-07 15:18:28 +0530

    View,Text,StyleSheet components

--- rn-starter/App.jsx
+++ rn-starter/App.jsx
@@ -1,14 +1,27 @@
-import { View, Text} from 'react-native';
+import { View, Text,StyleSheet} from 'react-native';
 import React from 'react';
 
 const App = () => {
   return (
-    <View>
-      <Text>First App Development</Text>
+    <View style ={styles.container}>
+    <Text style = {styles.text}>First App Development</Text>
     </View>
   );
 };
 
+const styles = StyleSheet.create(
+  {
+    container : {
 
+      justifyContent : 'center',
+      alignItems : 'center',
+      flex : 1,
+      backgroundColor : 'ffff'
+    },
+    text : {
+      fontSize : 24,
+      fontStyle: 'italic',
+    }
+  });
 
 export default App;
