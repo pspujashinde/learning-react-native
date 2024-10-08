e19eb11356d41819da9396f74b2ee213a8bfd995
pspujashinde <pspujashinde@gmail.com>
2024-10-08 18:30:19 +0530

    JSX

--- rn-starter/App.jsx
+++ rn-starter/App.jsx
@@ -1,52 +1,15 @@
 import { View, Text,StyleSheet, Button,Alert} from 'react-native';
 import React from 'react';
 
-const App = () => {
-const handlePress = () => {
+const JSX = () =>{
+    let name ='Pooja';
+    let sirname ='Sathe';
 
-  Alert.alert('thank you');
-
-};
-  return (
-    <View style ={styles.container}>
-    <Text style = {styles.text}>Select Gender</Text>
-    <Button
-      title="Female"
-      onPress={handlePress}        // Event handler
-      type="button"                // Type of button (button, submit, reset)
-      disabled={false}             // Disable the button
-      style={{ backgroundColor: 'blue', color: 'white' }} // Inline styles
-      className="my-button-class"  // CSS class name
-      aria-label="My button"       // Accessibility label
-    >
-    </Button>
-    <Button
-      title="Male"
-      onPress={handlePress}        // Event handler
-      type="button"                // Type of button (button, submit, reset)
-      disabled={false}             // Disable the button
-      style={{ backgroundColor: 'blue', color: 'white' }} // Inline styles
-      className="my-button-class"  // CSS class name
-      aria-label="My button"       // Accessibility label
-    >
-    </Button>
+return(
+  <View>
+    <Text style={{fontSize : 30}}>{name}</Text>
+    <Text style={{fontSize : 30}}>{sirname}</Text>
     </View>
-  );
+);
 };
-
-const styles = StyleSheet.create(
-  {
-    container : {
-
-      justifyContent : 'center',
-      alignItems : 'center',
-      flex : 1,
-      backgroundColor : 'ffff'
-    },
-    text : {
-      fontSize : 24,
-      fontStyle: 'italic',
-    }
-  });
-
-export default App;
+export default JSX;
