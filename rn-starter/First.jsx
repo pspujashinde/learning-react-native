31a411f2c2890f0b9b93eaef9b5c2f0a685e5da1
pspujashinde <pspujashinde@gmail.com>
2024-10-08 17:44:28 +0530

    component reusable

---
+++ rn-starter/First.jsx
@@ -0,0 +1,12 @@
+import react from "react";
+import { Text,View } from "react-native";
+const First = () => {
+return(
+<View>
+    <Text>This is first component </Text>
+</View>
+
+);
+};
+
+export default First;
