65a948e8b717a43a1a67203fe2d340dbc774ece9
pspujashinde <pspujashinde@gmail.com>
2024-12-01 15:27:39 +0530

    Lifecycle with UseEffect

--- rn-starter/App.js
+++ rn-starter/App.js
@@ -1,73 +1,22 @@
-import { View, Text,StyleSheet,TextInput, Button,Alert} from 'react-native';
-import React ,{useState}from 'react';
+import React, { useEffect, useState } from "react";
+import { Text,Button, View } from "react-native";
+
+const App=()=>{
+const [count,setcount]=useState(0); 
+useEffect(()=>{
+
+  console.warn("Hello");
+},[])
 
 
-const App = () =>{
-
-
-  const[name,setname]=useState('');
-  const[email,setemail]=useState('');
-  const[password,setpassword]=useState('');
-  
-
-  const handleSubmit = () => {
-   
-    if (!name || !email || !password) {
-      Alert.alert('Error', 'Please fill in all fields!');
-      
-    }
-  else{
-  
-  Alert.alert('Success', 'Form Submitted!');
- 
-  }
-  };
-
-  const AllClear = () =>
-  {
-  //setdisplay(false);
-  setname('');
-  setemail('');
-  setpassword('');
-  }
 return(
-  <View>
-    <Text style={{fontSize:20,margin:30}}>Please fill the below form</Text>
-    <TextInput
-    style={{fontSize:30,borderColor:'blue',borderWidth:2,margin:40}}
-    placeholder='Enter  full  name'
-    value={name}
-    onChangeText={(text)=>setname(text)}
-    />
+<View>
+<Text style={{fontSize:30}}>Lifecycle with UseEffect count:{count}</Text>
+<Button title="UpdateCount" onPress={()=>{setcount(count+1)}}/>
 
-<TextInput
-    style={{fontSize:30,borderColor:'blue',borderWidth:2,margin:40}}
-    placeholder='Enter your Email'
-    value={email}
-    onChangeText={(text)=>setemail(text)}
-    />
+</View>
 
-<TextInput
-    style={{fontSize:30,borderColor:'blue',borderWidth:2,margin:40}}
-    placeholder='Enter your Password'
-    value={password}
-    secureTextEntry={true}
-    onChangeText={(text)=>setpassword(text)}
-    />
-
-    
-  <Button title='Submit'onPress={handleSubmit}/>
-  {/* Conditionally Render Display Information */}
-  
-  <Button title='Clear'onPress={AllClear}/>
-
-
-
-  </View>
-  
-);
-
-
-};
+  )
+}
 
 export default App;
