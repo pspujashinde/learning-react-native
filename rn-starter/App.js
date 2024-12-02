73e07e1c93433fc39c7a0d68d07d26db99adc95d
pspujashinde <pspujashinde@gmail.com>
2024-12-02 20:51:27 +0530

    Work with Flex

--- rn-starter/App.js
+++ rn-starter/App.js
@@ -1,37 +1,39 @@
-import React, { useEffect, useState } from "react";
-import { Text,Button, View } from "react-native";
-
-const App=()=>{
-const[show,setshow]=useState(true)
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 
-return(
-<View>
-<Text style={{fontSize:30}}>useEffect with unmount life cycle</Text>
-<Button title="Toggle"style={{fontSize:20}} onPress={()=>setshow(!show)}/>
 
-{
-  show ?<Student/> :null
-}
+const App = () => {
 
-</View>
 
+
+  return (
+    <View style={styles.main}>
+    <View style={styles.box1}></View>
+    <View style={styles.box2}></View>
+    <View style={styles.box3}></View>
+    <View style={styles.box4}></View>
+    </View>
   )
 }
 
+const styles=StyleSheet.create({
+  main:{
+    backgroundColor: 'yellow',flex: 1
+  },
+  box1:
+  {
+    backgroundColor: 'red',flex: 1
+  },
+  box2:{
+    backgroundColor: 'blue',flex: 1
+  },
+  box3:{
+    backgroundColor: 'green',flex: 1
+  },
+  box4:{
+    backgroundColor: 'grey',flex: 1
+  }
+})
 
-const Student=()=>{
-
-  let timer=setInterval(()=>
-  {},2000);
-
-  useEffect(()=>{
-
-    return()=>clearInterval(timer);
-  })
-
-  return(
-    <Text style={{fontSize:30}}>Student's Data</Text>
-  )
-}
 export default App;
