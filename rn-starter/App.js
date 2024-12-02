b651d4621c2b0abc8afdb4c448cae7c4bf9a5678
pspujashinde <pspujashinde@gmail.com>
2024-12-02 19:52:09 +0530

    After hiding component,Need to Clear timer using useEffect

--- rn-starter/App.js
+++ rn-starter/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Text,Button, View } from "react-native";
 
 const App=()=>{
@@ -7,7 +7,7 @@ const[show,setshow]=useState(true)
 
 return(
 <View>
-<Text style={{fontSize:40}}>Toggle component</Text>
+<Text style={{fontSize:30}}>useEffect with unmount life cycle</Text>
 <Button title="Toggle"style={{fontSize:20}} onPress={()=>setshow(!show)}/>
 
 {
@@ -20,8 +20,16 @@ return(
 }
 
 
-const Student=()=>
-{
+const Student=()=>{
+
+  let timer=setInterval(()=>
+  {},2000);
+
+  useEffect(()=>{
+
+    return()=>clearInterval(timer);
+  })
+
   return(
     <Text style={{fontSize:30}}>Student's Data</Text>
   )
