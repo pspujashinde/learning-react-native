2d2ff9d89f22ea4e4b9dac8a5b80d30eab821eae
pspujashinde <pspujashinde@gmail.com>
2024-12-04 16:45:36 +0530

    ActivityIndicator

--- rn-starter/App.js
+++ rn-starter/App.js
@@ -1,47 +1,27 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, TouchableHighlight, TouchableOpacity, View } from "react-native";
+import { StyleSheet, Text, TouchableHighlight, TouchableOpacity, View ,ActivityIndicator, Button} from "react-native";
 
 
 
 const App = () => {
-const Skill=[
- { 
-  id:1,
-  name:'java',
- },
- {
- id:2,
- name:'php'
- },
- {
-  id:3,
-  name:'NodeJs'
- },
- {
-  id:4,
-  name:'angular'
- }
 
-]
-const[selectedradio,setselectedradio]=useState(1)
+const[show,setshow]=useState(false)
+
+const DisplayIndicator=()=>{
+  setshow(true);
+
+
+
+setTimeout=(()=>{
+    setshow(false);
+  },5000);
+}
+
 
   return (
     <View style={styles.main}>
-  
-    {
-      Skill.map((item)=><TouchableOpacity onPress={()=>setselectedradio(item.id)}>
-      <View style={styles.radiowraper}>
-      <View style={styles.circle}>
-        
-      {
-        selectedradio==item.id?<View style={styles.circlebg}></View>:null
-      }
-      </View>
-      <Text style={styles.radioText}>{item.name}</Text>
-      </View>
-    </TouchableOpacity>)
-    }
-    
+<ActivityIndicator size="large"color="red" animating={show}/>
+<Button title="submit" onPress={DisplayIndicator}/>
     </View>
 
     
@@ -54,41 +34,8 @@ const styles=StyleSheet.create({
     alignItems:'center',
     justifyContent:'center',
 
-  },
-  circle:{
-    
-    height:40,
-    width:40,
-    borderRadius:20,
-    borderWidth:2,
-    borderColor:'black',
-    margin:10,
-    
-    
-  },
-  circlebg:{
-    backgroundColor:'black',
-    height:28,
-    width:28,
-    borderRadius:20,
-    margin:4,
+  }
   
-
-  },
-  radioText:{
-    
-    color:'black',
-    fontSize:26,
-    padding:10,
-    margin:20,
-    textAlign:'center',
-    
-    
-  },
-  radiowraper:{
-    flexDirection:'row',
-    alignItems:'center',
-  },
   
 })
 
