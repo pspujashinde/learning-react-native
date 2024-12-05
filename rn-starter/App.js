a074365ac7166558d186dd17d40d7733f2eefb22
pspujashinde <pspujashinde@gmail.com>
2024-12-05 23:31:25 +0530

    WebView

--- rn-starter/App.js
+++ rn-starter/App.js
@@ -1,64 +1,14 @@
-import React, { useState } from "react";
-import { StyleSheet, Text, Modal, Button,View} from "react-native";
-
+import React from "react";
+import{WebView} from "react-native-webview"
 
 
 const App = () => {
 
-const[showmodal,setshowmodal]=useState(false)
 
 
-  return (
-<View style={styles.main}>
-  <Modal 
-   transparent={true}
-   visible={showmodal}
-   animationType={"slide"}
-   >
-    <View style={styles.centerView}>
-      <View style={styles.modelView}>
-        <Text style={{fontSize:20,marginBottom:20}}>Hello this is dialog box</Text>
-        <Button title="close" onPress={()=>setshowmodal(false)}/>
-      </View>
-    </View>
-  </Modal>
-<View style={styles.buttonView}>
-<Button title="Open modal" onPress={()=>setshowmodal(true)}/>
-</View>
-    
-    </View>
 
-    
-  )
+  return (<WebView source={{uri:"https://reactnative.dev/docs/components-and-apis"}} />);
 }
 
-const styles=StyleSheet.create({
-  main:{
-    flex:1,
-    alignItems:'center',
-    justifyContent:'center',
-
-  },
-  buttonView:
-  {
-    flex:1,
-    justifyContent:'flex-end',
-
-  },
-  centerView:
-  {
-flex:1,
-justifyContent:'center',
-alignItems:'center',
-  },
-  modelView:{
-    backgroundColor:'skyblue',
-    padding:30,
-    borderRadius:20,
-    elevation:5,
-    shadowOpacity:5,
-  }
-  
-})
 
 export default App;
