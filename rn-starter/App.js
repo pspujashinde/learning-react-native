8af1ad9a2e76dd1207dc9060ed165906c21ec815
pspujashinde <pspujashinde@gmail.com>
2024-12-06 22:51:36 +0530

    Stack navigation(Login->Home->Addnew)

--- rn-starter/App.js
+++ rn-starter/App.js
@@ -1,51 +1,65 @@
 import * as React from 'react';
-import { Button, Text,View } from 'react-native';
+import { Button, Text, TextInput, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import {createStackNavigator}from '@react-navigation/stack';
+import { createStackNavigator } from '@react-navigation/stack';
 
 const Stack = createStackNavigator();
 
-const  App=()=> {
+const App = () => {
   return (
     <NavigationContainer>
-     <Stack.Navigator initialRouteName="Login"
-     screenOptions={{
-  
-      headerStyle:{
-        backgroundColor:'skyblue',
-        },
-        headerTintColor:'Black',
-        headerTitleStyle:{
-        fontSize:20,
-      }}}>
-        <Stack.Screen name="Login" component={Login} 
+      <Stack.Navigator
+        initialRouteName="Login"
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: 'skyblue',
+          },
+          headerTintColor: 'Black',
+          headerTitleStyle: {
+            fontSize: 20,
+          },
+        }}
+      >
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Addnew" component={Addnew} />
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={({ navigation }) => ({
+            headerTitle: () => <Text>MyCouponBag</Text>,
+            headerRight: () => (
+              <Button title="Add New" onPress={() => navigation.navigate('Addnew')} />
+            ),
+          })}
         />
-
-
-        <Stack.Screen name="Home" component={Home}/>
-        </Stack.Navigator>
+      </Stack.Navigator>
     </NavigationContainer>
   );
-}
+};
 
-const Home=()=>{
+const Addnew = () => {
+  return (
+    <View>
+      <TextInput placeholder="Enter your coupon details" />
+    </View>
+  );
+};
 
-  return(
-    
-      <View>
-        <Text>Home Screen</Text>
-      </View>
-  )
-}
-const Login=(props)=>{
+const Home = () => {
+  return (
+    <View>
+      <Text>Home Screen</Text>
+    </View>
+  );
+};
 
-  return(
-    
-      <View>
-        <Text>Login Screen</Text>
-        <Button title='Go to Home page' onPress={()=>props.navigation.navigate("Home")}/>
-      </View>
-  )
-}
+const Login = ({ navigation }) => {
+  return (
+    <View>
+      <Text>Login Screen</Text>
+      <Button title="Go to Home page" onPress={() => navigation.navigate('Home')} />
+    </View>
+  );
+};
 
 export default App;
