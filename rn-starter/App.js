91031bcd4f15a781b08de2c931ee8efbe0791ac5
pspujashinde <pspujashinde@gmail.com>
2024-12-06 20:01:18 +0530

    Stack-Navigation

--- rn-starter/App.js
+++ rn-starter/App.js
@@ -1,14 +1,39 @@
-import React from "react";
-import{WebView} from "react-native-webview"
+import * as React from 'react';
+import { Button, Text,View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import {createStackNavigator}from '@react-navigation/stack';
 
+const Stack = createStackNavigator();
 
-const App = () => {
-
-
-
-
-  return (<WebView source={{uri:"https://reactnative.dev/docs/components-and-apis"}} />);
+const  App=()=> {
+  return (
+    <NavigationContainer>
+     <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={Login}/>
+        <Stack.Screen name="Home" component={Home}/>
+        </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
+const Home=()=>{
+
+  return(
+    
+      <View>
+        <Text>Home Screen</Text>
+      </View>
+  )
+}
+const Login=(props)=>{
+
+  return(
+    
+      <View>
+        <Text>Login Screen</Text>
+        <Button title='Go to Home page' onPress={()=>props.navigation.navigate("Home")}/>
+      </View>
+  )
+}
 
 export default App;
