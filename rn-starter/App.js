636d9460ad004bca3a7ca0a8c221300f5d8dabe8
pspujashinde <pspujashinde@gmail.com>
2024-12-10 15:29:10 +0530

    map funtion and List with API Call

--- rn-starter/App.js
+++ rn-starter/App.js
@@ -1,4 +1,4 @@
-import {  Text, View } from 'react-native';
+import {  Text, View,ScrollView } from 'react-native';
 import { useEffect, useState } from 'react';
 
 const App = () => {
@@ -6,9 +6,9 @@ const App = () => {
 
   const getapicall = async () => {
     // API Call
-    const url = "https://jsonplaceholder.typicode.com/posts/1";
+    const url = "https://jsonplaceholder.typicode.com/comments?postId=1";
     const result = await fetch(url);
-    const data = await result.json(); // Declare 'Final' here
+    const data = await result.json();
     setdata(data);
   };
 
@@ -18,16 +18,28 @@ const App = () => {
 
   return (
     <View>
-      <Text>API CALL</Text>
-      {data ? (
-        <View>
-          <Text>{data.userId}</Text>
-          <Text>{data.id}</Text>
-          <Text>{data.title}</Text>
-          <Text>{data.body}</Text>
-        </View>
-      ) : null}
-    </View>
+      <Text>map funtion and List with API Call  </Text>
+    
+   <ScrollView>
+            {
+              data.length ?
+
+              data.map((item)=>
+                      <View>
+                        <Text>{item.id}</Text>
+                        <Text>{item.name}</Text>
+                        <Text>{item.email}</Text>
+                        <Text>{item.body}</Text>
+                      </View>
+              
+                      )
+
+              :null
+
+            }
+
+   </ScrollView>
+   </View>
   );
 };
 
